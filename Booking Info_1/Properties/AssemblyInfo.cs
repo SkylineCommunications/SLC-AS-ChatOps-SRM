ca1623839e989ff16532fe5afc77053f0e855527
Joey Vanhalst <joey.vanhalst@skyline.be>
2023-05-31 10:50:49 +0200

    initial solution

---
+++ Booking Info_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Booking Info_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Booking Info_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8DF855B3-19C7-4572-A112-610B0EBE61B4")]
+[assembly: AssemblyVersion("1.0.0.0")]
